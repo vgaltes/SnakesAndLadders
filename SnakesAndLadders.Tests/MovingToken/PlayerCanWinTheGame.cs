4e0257790525cadc0669fcb076af249c2873bae2
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-10-30 17:35:33 +0000

    Refactor tests.

--- SnakesAndLadders.Tests/MovingToken/PlayerCanWinTheGame.cs
+++ SnakesAndLadders.Tests/MovingToken/PlayerCanWinTheGame.cs
@@ -6,15 +6,22 @@ namespace SnakesAndLadders.Tests
   public class PlayerCanWinTheGame
   {
     const int LAST_SQUARE = 100;
+    Mock<IBoardPrinter> boardPrinter;
+    Mock<IDice> dice;
+    Game game;
+
+    [SetUp]
+    public void SetUp() {
+      boardPrinter = new Mock<IBoardPrinter>();
+      dice = new Mock<IDice>();
+      game = new Game(boardPrinter.Object, dice.Object);
+      game.Start();
+    }
 
     [Test]
     public void WhenThePlayerGetsToTheLastSquareSheWinsTheGame(){
-      var boardPrinter = new Mock<IBoardPrinter>();
-      var dice = new Mock<IDice>();
-      var game = new Game(boardPrinter.Object, dice.Object);
-      game.Start();
-
       dice.SetupSequence(d => d.Roll()).Returns(LAST_SQUARE - 4).Returns(3);
+
       game.Move();
       game.Move();
       game.PrintBoard();
@@ -25,12 +32,8 @@ namespace SnakesAndLadders.Tests
     [Test]
     public void WhenThePlayerIsSupposedToMoveFurtherThanTheFinalSquareSheDoesNotMove()
     {
-      var boardPrinter = new Mock<IBoardPrinter>();
-      var dice = new Mock<IDice>();
-      var game = new Game(boardPrinter.Object, dice.Object);
-      game.Start();
-
       dice.SetupSequence(d => d.Roll()).Returns(LAST_SQUARE - 4).Returns(4);
+
       game.Move();
       game.Move();
       game.PrintBoard();
@@ -38,5 +41,4 @@ namespace SnakesAndLadders.Tests
       boardPrinter.Verify(p => p.Print(It.Is<Token>(t => t.Square == 97 && t.State == TokenState.Playing)));
     }
   }
-
 }
