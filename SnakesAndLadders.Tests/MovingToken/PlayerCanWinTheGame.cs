3b59854abb734d5ca070246470bc249b97499178
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-10-30 17:14:36 +0000

    WhenThePlayerIsSupposedToMoveFurtherThanTheFinalSquareSheDoesNotMove failing

--- SnakesAndLadders.Tests/MovingToken/PlayerCanWinTheGame.cs
+++ SnakesAndLadders.Tests/MovingToken/PlayerCanWinTheGame.cs
@@ -19,6 +19,19 @@ namespace SnakesAndLadders.Tests
 
       boardPrinter.Verify(p => p.Print(It.Is<Token>(t => t.Position == 100 && t.State == TokenState.Winner)));
     }
+
+    [Test]
+    public void WhenThePlayerIsSupposedToMoveFurtherThanTheFinalSquareSheDoesNotMove()
+    {
+      var boardPrinter = new Mock<IBoardPrinter>();
+      var game = new Game(boardPrinter.Object);
+      game.Start();
+      game.Move(LAST_SQUARE - 4);
+      game.Move(4);
+      game.PrintBoard();
+
+      boardPrinter.Verify(p => p.Print(It.Is<Token>(t => t.Position == 97 && t.State == TokenState.Playing)));
+    }
   }
 
 }
